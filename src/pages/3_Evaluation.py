172d111853623f1ff113834e6254ef33672e08ab
Hanyu Wu <whanyu1212@hotmail.com>
2023-10-29 22:32:18 +0800

    added complementary metrics

--- src/pages/3_Evaluation.py
+++ src/pages/3_Evaluation.py
@@ -8,7 +8,14 @@ from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode, DataReturnMode
 from streamlit_lottie import st_lottie
 from util.markdown_latex import evaluation_description
 from util.sidebar import generate_sidebar
-from util.functions import compute_NPV, compute_IRR, compute_payback_period
+from util.functions import (
+    compute_NPV,
+    compute_IRR,
+    compute_payback_period,
+    extend_cashflows_for_LCM,
+    calculate_EAA,
+    profitability_index,
+)
 from util.create_df import (
     compute_financials_planet,
     transpose_and_format_planet,
@@ -17,7 +24,9 @@ from util.create_df import (
     normalize_values,
 )
 import plotly.graph_objects as go
-
+import plotly.express as px
+import numpy as np
+import statsmodels.api as sm
 
 st.set_page_config(layout="wide")
 st_lottie(
@@ -122,6 +131,171 @@ st.plotly_chart(fig, use_container_width=True, theme="streamlit")
 st.info(evaluation_description)
 st.text("")
 st.subheader("Complementary Metrics")
-tab1, tab2, tab3 = st.tabs(
-    ["Sensitivity Analysis", "Lowest Common Multiple", "Equivalent Annual Annuity"]
+tab1, tab2, tab3, tab4 = st.tabs(
+    [
+        "Sensitivity Analysis",
+        "Least Common Multiple",
+        "Equivalent Annual Annuity",
+        "Profitability Index",
+    ]
 )
+
+with tab1:
+    wacc_values = np.linspace(0.05, 0.2, 100)  # WACC range from 5% to 20%
+    npvs_project1 = []
+    npvs_project2 = []
+
+    # Compute NPV for each WACC for both projects
+    for w in wacc_values:
+        npvs_project1.append(compute_NPV(planet_cash_flow, w))
+        npvs_project2.append(compute_NPV(beach_cash_flow, w))
+
+    # Plot with Plotly for both projects
+    fig = go.Figure()
+
+    # Project 1 NPVs
+    fig.add_trace(
+        go.Scatter(
+            x=wacc_values,
+            y=npvs_project1,
+            mode="lines+markers",
+            name="Project 1",
+            line=dict(width=3, color="blue"),
+        )
+    )
+
+    # Project 2 NPVs
+    fig.add_trace(
+        go.Scatter(
+            x=wacc_values,
+            y=npvs_project2,
+            mode="lines+markers",
+            name="Project 2",
+            line=dict(width=3, color="red"),
+        )
+    )
+
+    fig.update_layout(
+        title="NPV Sensitivity to WACC for Two Projects",
+        xaxis_title="WACC",
+        yaxis_title="Net Present Value (NPV)",
+    )
+
+    st.plotly_chart(fig, use_container_width=True, theme="streamlit")
+    st.markdown(
+        """
+### Sensitivity of NPV to WACC: Interpretation
+
+From the graph above, we can derive the following insights:
+
+1. **Slope of the NPV curves**:
+   - The steeper the slope of the NPV curve with respect to changes in WACC, the more sensitive the project's NPV is to those changes. 
+   - In the provided chart, it appears that the NPV of **Beach Karaoke Pub** (red line) has a steeper slope compared to **Planet Karaoke Pub** (blue line). This indicates that **Beach Karaoke Pub's NPV is more sensitive to changes in WACC** than Planet Karaoke Pub's NPV.
+
+2. **Implication of Sensitivity**:
+   - A project's sensitivity to changes in WACC provides insights into its risk profile.
+     - **Higher Sensitivity (Steeper Slope)**: If a project's NPV is highly sensitive to WACC changes, it implies that small changes in the project's cost of capital can result in significant fluctuations in its value. Such a project might be considered **more risky**, as its profitability is more susceptible to changes in the financing environment.
+     - **Lower Sensitivity (Gentler Slope)**: Conversely, a project with a more gradual slope (i.e., less sensitivity to WACC) indicates that the project's value is relatively stable against changes in the cost of capital. This could be interpreted as the project being **less risky** in terms of its financing environment.
+
+In conclusion, based on the provided graph, **Beach Karaoke Pub's NPV is more sensitive to changes in WACC** than Planet Karaoke Pub's NPV. This suggests that Beach Karaoke Pub might have a higher financial risk profile compared to Planet Karaoke Pub, given that its value is more susceptible to fluctuations in the cost of capital. On the other hand, Planet Karaoke demonstrates a more stable value proposition against changes in its financing conditions.
+"""
+    )
+
+with tab2:
+    extended_planet_cashflows = extend_cashflows_for_LCM(planet_cash_flow, 12)
+    extended_beach_cashflows = extend_cashflows_for_LCM(beach_cash_flow, 12)
+
+    planet_npv_extended = compute_NPV(extended_planet_cashflows, wacc)
+    beach_npv_extended = compute_NPV(extended_beach_cashflows, wacc)
+
+    df_long = pd.DataFrame(
+        {
+            "Project": [
+                "Planet Karaoke Pub",
+                "Planet Karaoke Pub",
+                "Beach Karaoke Pub",
+                "Beach Karaoke Pub",
+            ],
+            "NPV": [planet_npv, planet_npv_extended, beach_npv, beach_npv_extended],
+            "Metric": ["Original", "Extended", "Original", "Extended"],
+        }
+    )
+
+    fig = px.bar(
+        df_long,
+        x="Project",
+        y="NPV",
+        color="Metric",
+        barmode="group",
+        text="NPV",
+        title="NPV for Both Projects",
+    )
+    st.plotly_chart(fig, use_container_width=True, theme="streamlit")
+    st.markdown(
+        """
+**In conducting an LCM analysis** for projects with differing durations, 
+we aim to provide a standardized comparison over a **common time horizon**. 
+By extending the cash flows of each project to match the **LCM duration**, 
+we essentially simulate multiple cycles of the project without repeating the initial investment. 
+The extended cash flows allow us to calculate metrics like **NPV** over the same time frame, 
+offering a more equitable basis for comparison. 
+"""
+    )
+
+with tab3:
+    planet_eaa = calculate_EAA(planet_npv, wacc, 4)
+    beach_eaa = calculate_EAA(beach_npv, wacc, 6)
+    df_plot = pd.DataFrame(
+        {
+            "Project": ["Planet Karaoke Pub", "Beach Karaoke Pub"],
+            "EAA": [planet_eaa, beach_eaa],
+        }
+    )
+
+    fig = px.bar(
+        df_plot,
+        x="Project",
+        y="EAA",
+        text="EAA",
+        color="Project",
+        title="Equivalent Annual Annuity",
+    )
+    st.plotly_chart(fig, use_container_width=True, theme="streamlit")
+    st.markdown(
+        """The Effective Annual Annuity (EAA) is a valuable metric that allows us to convert the net present value (NPV) of a project into an equivalent constant yearly cash flow over the project's life. By analyzing the EAA, we can discern the uniform annual amount an investor can expect to receive over the project duration, making it easier to compare projects of varying lengths and cash flow patterns. In our analysis, a higher EAA indicates a project that, on an annualized basis, delivers superior value, enabling stakeholders to make informed decisions grounded in consistent, annualized terms"""
+    )
+with tab4:
+    planet_pi = profitability_index(planet_cash_flow, wacc)
+    beach_pi = profitability_index(beach_cash_flow, wacc)
+    fig = go.Figure(
+        go.Scatter(x=[0, 1], y=[0, 1], mode="markers", marker=dict(opacity=0))
+    )
+
+    # Add annotations (numbers) for the PI of both projects
+    fig.add_annotation(
+        x=0.25,
+        y=0.5,
+        text=f"Project A<br>PI: {planet_pi}",
+        showarrow=False,
+        font=dict(size=24),
+    )
+
+    fig.add_annotation(
+        x=0.75,
+        y=0.5,
+        text=f"Project B<br>PI: {beach_pi}",
+        showarrow=False,
+        font=dict(size=24),
+    )
+
+    # Update layout to hide axis and set title
+    fig.update_layout(
+        xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+        yaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+        title="Comparison of Profitability Index (PI)",
+    )
+
+    st.plotly_chart(fig, use_container_width=True, theme="streamlit")
+    st.markdown(
+        """The Profitability Index (PI) serves as a crucial indicator of a project's potential return in relation to its initial investment. Essentially, it quantifies the present value of future cash flows per unit of investment, allowing us to gauge the relative value delivered by each project. A PI greater than 1 suggests that the project is expected to generate more value than its initial cost, making it a viable investment option. Conversely, a PI less than 1 indicates that the project might not yield adequate returns compared to its investment. By comparing the PIs of two projects, we can swiftly determine which project offers better value for every dollar invested, facilitating more informed and strategic investment decisions. """
+    )
--- src/util/functions.py
+++ src/util/functions.py
@@ -60,3 +60,69 @@ def compute_payback_period(cash_flows):
             return i + 1
     # If payback period is not achieved within the given period
     return None
+
+
+def extend_cashflows_for_LCM(cash_flows, lcm_duration):
+    """
+    Extend cash flows for the LCM duration without repeating the initial investment.
+
+    Parameters:
+    - cash_flows (list): An array of cash flows where the index represents the time period (starting from 0).
+    - lcm_duration (int): The LCM duration over which the project should be extended.
+
+    Returns:
+    - list: The extended cash flows over the LCM duration.
+    """
+
+    # Find out how many times the project needs to be repeated
+    repetitions = lcm_duration // (len(cash_flows) - 1)
+
+    # Extend the cash flows without the initial investment
+    extended_cashflows = [cash_flows[0]]  # Include initial investment only once
+    for _ in range(repetitions):
+        extended_cashflows.extend(cash_flows[1:])
+
+    return extended_cashflows
+
+
+def calculate_EAA(npv, discount_rate, n):
+    """
+    Calculate the Effective Annual Annuity (EAA) based on the NPV of a project.
+
+    Parameters:
+    - npv (float): Net Present Value of the project's cash flows.
+    - discount_rate (float): The discount rate used for NPV calculations (e.g., 0.05 for 5%).
+    - n (int): The number of years or lifespan of the project.
+
+    Returns:
+    - float: The Effective Annual Annuity (EAA).
+    """
+
+    annuity_factor = discount_rate / (1 - (1 + discount_rate) ** -n)
+    eaa = npv * annuity_factor
+
+    return eaa
+
+
+def profitability_index(cash_flows, discount_rate):
+    """
+    Compute the Profitability Index given an array of cash flows and a discount rate.
+
+    Parameters:
+    - cash_flows (list or array): An array of cash flows where the first value is the initial investment (negative),
+      and the subsequent values are expected cash inflows.
+    - discount_rate (float): Discount rate used to calculate the present value of future cash flows.
+
+    Returns:
+    - float: Profitability Index.
+    """
+
+    # Calculate the present value of each cash inflow
+    discounted_cashflows = [
+        cf / (1 + discount_rate) ** i for i, cf in enumerate(cash_flows)
+    ]
+
+    # The PV of expected future cash inflows is the sum of discounted cashflows minus the initial investment
+    pv_cash_inflows = np.sum(discounted_cashflows) - cash_flows[0]
+
+    return pv_cash_inflows / -cash_flows[0]
